af1ba446d62431f70058840b559b94b0fc297f9e
oneonetwo <ceanpusa@gmail.com>
2024-05-29 00:34:20 +0800

    Update Main.java

--- ewallet/Main.java
+++ ewallet/Main.java
@@ -1,7 +1,8 @@
 import javax.swing.SwingUtilities;
 
 // The main method to start the application
+public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(SplashScreen::new);  // Create and show the splash screen on the Event Dispatch Thread
+        SwingUtilities.invokeLater(SplashScreen::new); // Create and show the splash screen on the Event Dispatch Thread
     }
 }
